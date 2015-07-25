0xlab/0xdroid-frameworks_base

libagl: Optimize matrix multiply using ARM VFP
We can use ARMv6 VFP to optimize matrix multiply manipulation in libagl.

Use angles as test benchmark program. Here are the reference results per
1000 times on TI OMAP3 600 MHz (Cortex A8) target:

[before]
D/StopWatch( 3018): StopWatch? matrixf_t::multiply (us): 1983
D/StopWatch( 3018): StopWatch? matrixf_t::multiply (us): 2075
D/StopWatch( 3018): StopWatch? matrixf_t::multiply (us): 1953
D/StopWatch( 3018): StopWatch? matrixf_t::multiply (us): 1922
D/StopWatch( 3018): StopWatch? matrixf_t::multiply (us): 1953
D/StopWatch( 3018): StopWatch? matrixf_t::multiply (us): 2075
D/StopWatch( 3018): StopWatch? matrixf_t::multiply (us): 1953
Avg = 1987.71 ns

[after]
D/StopWatch( 3070): StopWatch? matrixf_t::multiply (us): 1739
D/StopWatch( 3070): StopWatch? matrixf_t::multiply (us): 1892
D/StopWatch( 3070): StopWatch? matrixf_t::multiply (us): 1739
D/StopWatch( 3070): StopWatch? matrixf_t::multiply (us): 1770
D/StopWatch( 3070): StopWatch? matrixf_t::multiply (us): 1739
D/StopWatch( 3070): StopWatch? matrixf_t::multiply (us): 1892
D/StopWatch( 3070): StopWatch? matrixf_t::multiply (us): 1739
Avg 1787.14 ns

Improvement: 10%

opengl/libagl/Android.mk
@@ -32,6 +32,9 @@ LOCAL_LDLIBS := -lpthread -ldl
 ifeq ($(TARGET_ARCH),arm)
 	LOCAL_SRC_FILES += fixed_asm.S iterators.S
 	LOCAL_CFLAGS += -fstrict-aliasing
+ifeq ($(ARCH_ARM_HAVE_VFP),true)
+	LOCAL_CFLAGS += -DHAVE_ARM_VFP
+endif
 endif
 
 ifeq ($(ARCH_ARM_HAVE_TLS_REGISTER),true)

TeamEOS/frameworks_native

Change-Id: I053e0e4b72013aedb8016a6d87fe03c14ee62d85

opengl/libagl/Android.mk
@@ -31,6 +31,10 @@ LOCAL_SHARED_LIBRARIES := libcutils libhardware libutils liblog libpixelflinger 
 LOCAL_SRC_FILES_arm += fixed_asm.S iterators.S
 LOCAL_CFLAGS_arm += -fstrict-aliasing
 
+ifeq ($(ARCH_ARM_HAVE_VFP),true)
+	LOCAL_CFLAGS_arm += -DHAVE_ARM_VFP
+endif
+
 LOCAL_SRC_FILES_mips += arch-mips/fixed_asm.S
 LOCAL_CFLAGS_mips += -fstrict-aliasing
 # The graphics code can generate division by zero