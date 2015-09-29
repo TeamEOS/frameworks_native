5f4db0fe797a9a96099f7f505ac2173805075f0e
Glen D'souza <glndsouza2691995@gmail.com>
2015-09-29 18:47:30 +0530

    Damn the conservative 3gb values!
    Change-Id: I46cfa3b9fae5dfe256c42e38ebac661b28b39ef2

--- build/phone-xxhdpi-3072-dalvik-heap.mk
+++ build/phone-xxhdpi-3072-dalvik-heap.mk
@@ -18,9 +18,9 @@
 # 192m of RAM gives enough space for 5 8 megapixel camera bitmaps in RAM.
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapstartsize=24m \
+    dalvik.vm.heapgrowthlimit=320m \
+    dalvik.vm.heapsize=768m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
